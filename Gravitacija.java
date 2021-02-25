6421ce7534facd20663bf1586d9b71e73e53d1a4
nejcskrbec <nejc.skrbec@icloud.com>
2021-02-25 11:46:18 +0100

    Dodana funkcija za izracun in slika studenta

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,19 @@
-
+import java.util.*;
 
 public class Gravitacija {
     public static void main(String[] args) {
-        System.out.println("OIS je zakon");
+        Scanner sc = new Scanner(System.in);
+
+        double visina = sc.nextDouble();
+
+        System.out.println(pospesek(visina));
+    }
+
+    private static double pospesek(double nadmorskaVisina){
+        double G = 6.674 * Math.pow(10,-11);
+        double MASA_ZEMLJE = 5.972 * Math.pow(10,24);
+        double RADIJ_ZEMLJE = 6.371 * Math.pow(10,6);
+
+        return (G * MASA_ZEMLJE) / Math.pow((RADIJ_ZEMLJE + nadmorskaVisina), 2);
     }
 }
